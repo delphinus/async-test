5ad9297b3072b5b332b5b3154fdc109d948c9a1e
delphinus <delphinus@remora.cx>
2016-05-14 18:47:58 +0900

    Add getRandomPonyFooArticle()

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
 ///<reference path="./typings/main.d.ts" />
+///<reference path="./typings/others.d.ts" />
 
-import * as $ from "jquery";
+import * as request from "request";
+import * as hget from "hget";
+import * as marked from "marked";
+import * as Term from "marked-terminal";
+
+const getRandomPonyFooArticle: () => Promise<string> = () => new Promise((resolve, reject) => {
+    request("https://ponyfoo.com/articles/random", (err, res, body) => {
+        if (err) {
+            reject(err); return;
+        }
+        resolve(body);
+    });
+});
