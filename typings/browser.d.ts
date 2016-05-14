df6e15e369787b192bfc62d9eda51f552a8b6026
delphinus <delphinus@remora.cx>
2016-05-14 18:47:09 +0900

    Add typings

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="browser/ambient/form-data/index.d.ts" />
 /// <reference path="browser/ambient/jquery/index.d.ts" />
+/// <reference path="browser/ambient/node/index.d.ts" />
 /// <reference path="browser/ambient/request/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,2 +1,4 @@
+/// <reference path="main/ambient/form-data/index.d.ts" />
 /// <reference path="main/ambient/jquery/index.d.ts" />
+/// <reference path="main/ambient/node/index.d.ts" />
 /// <reference path="main/ambient/request/index.d.ts" />
